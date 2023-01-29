f6265f5a3381155c9925d4e22554092261469771
Andrew S Erwin <andrew@erwininteractive.com>
2023-01-29 00:19:25 -0600

    brackets on the next line

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -22,40 +22,32 @@ function createNextGenGrid(prevGrid)
 {
     let nextGrid = prevGrid.map((newarray) => [...newarray]);
 
-    for(let column = 0; column < prevGrid.length; column++)
-    {
-        for(let row = 0; row < prevGrid[column].length; row++)
-        {
+    for(let column = 0; column < prevGrid.length; column++) {
+        for(let row = 0; row < prevGrid[column].length; row++) {
             let cell = prevGrid[column][row];
             let neighbors = 0;
 
-            for(let i = -1; i < 2; i++)
-            {
-                for(let j = -1; j < 2; j++)
-                {
-                    if(i == 0 && j == 0)
-                    {
+            for(let i = -1; i < 2; i++) {
+                for(let j = -1; j < 2; j++) {
+                    if(i == 0 && j == 0) {
                         continue; /* nothing to see here */
                     }
 
                     let x = column + i;
                     let y = row + j;
 
-                    if(x >= 0 && y >=0 && x < columns && y < rows)
-                    {
+                    if(x >= 0 && y >=0 && x < columns && y < rows) {
                         let neighbor = prevGrid[column + i][row + j]
                         neighbors += neighbor;
                     }
                 }
             }
             
-            if(cell == 1 && (neighbors < 2 || neighbors > 3)) 
-            {
+            if(cell == 1 && (neighbors < 2 || neighbors > 3)) {
                 nextGrid[column][row] = 0;
             }
             
-            if(cell == 0 && neighbors == 3) 
-            {
+            if(cell == 0 && neighbors == 3) {
                 nextGrid[column][row] = 1;
             }
         }
@@ -66,10 +58,8 @@ function createNextGenGrid(prevGrid)
 
 function render(grid)
 {
-    for(let column = 0; column < grid.length; column++)
-    {
-        for(let row = 0; row < grid[column].length; row++)
-        {
+    for(let column = 0; column < grid.length; column++) {
+        for(let row = 0; row < grid[column].length; row++) {
             let cell = grid[column][row];
 
             ctx.beginPath();
